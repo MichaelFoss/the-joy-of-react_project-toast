59c3c821e27fabc34ffab32ebe2fdbbf473e01f1
Michael Foss <michael@mikefoss.com>
2024-10-16 23:46:16 -0400

    Allow Toast component to be managed by props

--- src/components/Toast/Toast.js
+++ src/components/Toast/Toast.js
@@ -18,16 +18,19 @@ const ICONS_BY_VARIANT = {
   error: AlertOctagon,
 };
 
-const Toast = () => {
+const Toast = ({ variant, message, onClose }) => {
+  const Component = ICONS_BY_VARIANT[variant];
+  const handleClose = () => {
+    onClose();
+  };
+
   return (
-    <div className={`${styles.toast} ${styles.notice}`}>
+    <div className={`${styles.toast} ${styles[variant] || ''}`}>
       <div className={styles.iconContainer}>
-        <Info size={24} />
+        <Component size={24} />
       </div>
-      <p className={styles.content}>
-        16 photos have been uploaded
-      </p>
-      <button className={styles.closeButton}>
+      <p className={styles.content}>{message}</p>
+      <button className={styles.closeButton} onClick={handleClose}>
         <X size={24} />
         <VisuallyHidden>Dismiss message</VisuallyHidden>
       </button>
