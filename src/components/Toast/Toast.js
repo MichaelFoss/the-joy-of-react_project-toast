628e41df45a2090bee265f508b7e50cbfd449707
Michael Foss <michael@mikefoss.com>
2024-10-16 23:46:29 -0400

    Handle bad variants

--- src/components/Toast/Toast.js
+++ src/components/Toast/Toast.js
@@ -20,6 +20,15 @@ const ICONS_BY_VARIANT = {
 
 const Toast = ({ variant, message, onClose }) => {
   const Component = ICONS_BY_VARIANT[variant];
+  if (!Component) {
+    const acceptedVariants = Object.keys(ICONS_BY_VARIANT);
+    throw new RangeError(
+      `${variant} is not accepted variant type of [${acceptedVariants.join(
+        ', '
+      )}].`
+    );
+  }
+
   const handleClose = () => {
     onClose();
   };
